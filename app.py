80b127512adbc5cad34a30e51a69b1b24699ccaa
serenity510 <sahraturner21@gmail.com>
2025-05-23 20:45:26 -0700

    Update app.py
    create_access_token, get_jwt_identity, jwt_required

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
+create_access_token, get_jwt_identity, jwt_required
 from flask_cors import CORS
 
 app = Flask(__name__)
